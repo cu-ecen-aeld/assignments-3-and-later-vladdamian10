1a66f5b33404776ceeb59292baffdc9a8f8cec83
vd10 <vd10@ubuntu2204.myguest.virtualbox.org>
2026-03-27 09:32:53 +0200

    assigment-3-part-2: Update the toods to build the linux kernel with qemu.

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -34,7 +34,13 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Done: Add your kernel build steps here
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all -j4
+    # skip building the modules
+    # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs 
 fi
 
 echo "Adding the Image in outdir"
@@ -47,7 +53,12 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Done: Create necessary base directoriesi
+mkdir -p "${OUTDIR}/rootfs"
+cd "${OUTDIR}/rootfs"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +66,59 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+    # Done:  Configure busybox
+    make distclean
+    make defconfig
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu-     
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Done: Make and install busybox
+make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- CONFIG_PREFIX="${OUTDIR}/rootfs" install
+
+cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Done: Add library dependencies to rootfs
+TOOLCHAIN_PATH=$(find ~/ -type d -iname "arm-gnu-toolchain-*")
+if [[ "${TOOLCHAIN_PATH}"x == ""x ]]; then
+  echo "Error: Path to toolchain is not found."
+else
+  # Would be fancier to find the files, instead of hardcoding the source path.
+  cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib/ld-linux-aarch64.so.1 ${OUTDIR}/rootfs/lib/
+  cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libm.so.6 ${OUTDIR}/rootfs/lib64/
+  cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libc.so.6 ${OUTDIR}/rootfs/lib64/
+  cp ${TOOLCHAIN_PATH}/aarch64-none-linux-gnu/libc/lib64/libresolv.so.2 ${OUTDIR}/rootfs/lib64/
+fi
 
-# TODO: Make device nodes
+# Done: Make device nodes
+sudo -i mknod -m 666 ${OUTDIR}/rootfs/dev/null c 1 3
+sudo -i mknod -m 666 ${OUTDIR}/rootfs/dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Done: Clean and build the writer utility
+cd "${FINDER_APP_DIR}"
+make clean
+make CROSS_COMPILE=aarch64-none-linux-gnu- all
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Done: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/finder.sh "${OUTDIR}/rootfs/home" 
+cp ${FINDER_APP_DIR}/finder-test.sh "${OUTDIR}/rootfs/home"
+cp ${FINDER_APP_DIR}/conf/* "${OUTDIR}/rootfs/home"
+cp ${FINDER_APP_DIR}/writer "${OUTDIR}/rootfs/home" 
+cp ${FINDER_APP_DIR}/autorun-qemu.sh "${OUTDIR}/rootfs/home" 
 
-# TODO: Chown the root directory
+# Correct the conf/assignment.txt path inside the destination file
+$(sed -i 's/\.\.\/conf\/assignment.txt/conf\/assignment.txt/g' "${OUTDIR}/rootfs/home/finder-test.sh")
 
-# TODO: Create initramfs.cpio.gz
+# Done: Chown the root directory
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+
+# Done: Create initramfs.cpio.gz
+gzip -f ${OUTDIR}/initramfs.cpio
+
