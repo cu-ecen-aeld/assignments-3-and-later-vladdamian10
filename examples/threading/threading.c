d32bf4a67063d41f9384fefe98d0857910b296c7
vd10 <vd10@ubuntu2204.myguest.virtualbox.org>
2026-04-02 22:53:10 +0300

    impl threadfunc

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,16 +4,46 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
 {
+    DEBUG_LOG("entering threadfunc");
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    if (thread_func_args != NULL) {
+       int usec = (useconds_t)(thread_func_args->wait_to_obtain_ms*1000);
+       if (usleep(usec) == 0) {
+           int rc = pthread_mutex_lock(thread_func_args->mutex);
+           if (rc != 0) {
+               printf("pthread_mutex_lock failed with %d\n", rc);
+               thread_func_args->thread_complete_success = false;
+           }
+
+           usec = (useconds_t)(thread_func_args->wait_to_release_ms*1000);
+           if (usleep(usec) != 0) {
+               DEBUG_LOG("Failed to usleep(wait_to_release_ms)");
+               thread_func_args->thread_complete_success = false;
+           }
+
+           rc = pthread_mutex_unlock(thread_func_args->mutex);
+           if (rc != 0) {
+               printf("pthread_mutex_unlock failed with %d\n", rc);
+               thread_func_args->thread_complete_success = false;
+           }
+           else {
+               thread_func_args->thread_complete_success = true;
+           }
+        }
+        else {
+            DEBUG_LOG("usleep(wait_to_obtain_ms) failed");
+            thread_func_args->thread_complete_success = false;
+        }
+    }
     return thread_param;
 }
 
