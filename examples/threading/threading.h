7d7ae9c1e18d0905ab3db4774dbfaa2fb4dad285
vd10 <vd10@ubuntu2204.myguest.virtualbox.org>
2026-04-02 22:52:33 +0300

    add thread_data struct members.

--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -9,12 +9,17 @@
  */
 struct thread_data{
     /*
-     * TODO: add other values your thread will need to manage
+     * Done: add other values your thread will need to manage
      * into this structure, use this structure to communicate
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
-
+    /* Number of ms to wait before locking */
+    int wait_to_obtain_ms;
+    /* Number of ms to wait in lock mode */
+    int wait_to_release_ms;
+    /* Ptr to mutex */
+    pthread_mutex_t* mutex;
     /**
      * Set to true if the thread completed with success, false
      * if an error occurred.
