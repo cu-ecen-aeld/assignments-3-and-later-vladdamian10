0424a6d70620e92568fe0b5e68d089d495b701a4
vlad <vladdamian10@gmail.com>
2026-04-21 09:51:19 +0200

    Move the send() logic to the loop which writes() to the file.

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -143,6 +143,24 @@ int main(int argc, char *argv[]) {
                             perror("write");
                             return -1;
                         } else {
+                            // TODO: Must look for a better place for this call.
+                            // Then send the buffer back to the client via the socket.
+                            lseek(fd, 0, SEEK_SET);
+                            char* readstr = (char*)malloc(BUFF_LEN_BYTES);
+                            ssize_t num_bytes_read;
+                            while ((num_bytes_read = read(fd, readstr, BUFF_LEN_BYTES)) > 0) {
+                                    ssize_t num_bytes_sent;
+                                    num_bytes_sent = send(new_sockfd, readstr, num_bytes_read, 0);
+                                    if (num_bytes_sent == -1) {
+                                        perror("send");
+                                        return -1;
+                                    }
+                            }
+                            if (num_bytes_read == -1) {
+                                perror("read");
+                                return -1;
+                            }
+                            free(readstr);
                             break;
                         }
                     }
@@ -161,24 +179,6 @@ int main(int argc, char *argv[]) {
                 // TODO: the remote side has closed the connection on the server.
                 // Should I exit?
             }
-            // TODO: Must look for a better place for this call.
-            // Then send the buffer back to the client via the socket.
-            lseek(fd, 0, SEEK_SET);
-            char* readstr = (char*)malloc(BUFF_LEN_BYTES);
-            ssize_t num_bytes_read;
-            while ((num_bytes_read = read(fd, readstr, BUFF_LEN_BYTES)) > 0) {
-                ssize_t num_bytes_sent;
-                num_bytes_sent = send(new_sockfd, readstr, num_bytes_read, 0);
-                if (num_bytes_sent == -1) {
-                    perror("send");
-                    return -1;
-                }
-            }
-            if (num_bytes_read == -1) {
-                perror("read");
-                return -1;
-            }
-            free(readstr);
 
             /* close the new socket for this connection */
             if (close(new_sockfd) == -1) {
