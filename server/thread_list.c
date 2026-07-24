7c4cd9a77dd985871faaed548676bff1d88dd0b8
vlad <vladdamian10@gmail.com>
2026-07-24 14:04:06 +0200

    add SLIST init(), add() and join() for thread management.

---
+++ server/thread_list.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <pthread.h>
+#include "thread_list.h"
+
+void thread_list_init(struct thread_list *head) {
+    SLIST_INIT(head);
+}
+
+int thread_list_add(struct thread_list *head, struct thread_data *td) {
+    struct thread_node *node = malloc(sizeof(struct thread_node));
+    if (node == NULL) {
+        return -1;
+    }
+    node->client = td;
+    // Add newer threads to the head, instead of traversing the list
+    // every time to find the last item. 
+    SLIST_INSERT_HEAD(head, node, next_node);
+    return 0;
+}
+
+void thread_list_join(struct thread_list *head, bool force_join) {
+    struct thread_node *cur, *tmp;
+    if (!force_join) { // join thread only if the work is complete.
+        SLIST_FOREACH_SAFE(cur, head, next_node, tmp) {
+            if (cur->client->is_complete) {
+                pthread_join(cur->client->thread_id, NULL);
+                SLIST_REMOVE(head, cur, thread_node, next_node);
+                free(cur->client);
+                free(cur);
+            }
+        }
+    }
+    else { // join thread no matter what.
+        SLIST_FOREACH_SAFE(cur, head, next_node, tmp) {
+            pthread_join(cur->client->thread_id, NULL);
+            SLIST_REMOVE(head, cur, thread_node, next_node);
+            free(cur->client);
+            free(cur);
+        }
+    }
+}
--- server/thread_list.h
+++ server/thread_list.h
@@ -1,6 +1,7 @@
 #if !defined(THREAD_LIST_H)
 #define THREAD_LIST_H
 
+#include <stdbool.h>
 #include <sys/queue.h>
 #include "thread_data.h"
 
@@ -11,4 +12,17 @@ struct thread_node {
 };
 
 SLIST_HEAD(thread_list, thread_node);
+
+void thread_list_init(struct thread_list *head);
+
+// Wraps td in a new thread_node and inserts it at the head of the list.
+// Returns 0 on success, -1 on allocation failure.
+int thread_list_add(struct thread_list *head, struct thread_data *td);
+
+// Joins and removes (and frees the node and its thread_data) entries,
+// depending on the @param force_join.
+// Set the @param force_join = true, when is required to join all the threads.
+// Otherwise, let the mechanics of the client decide when to join.
+void thread_list_join(struct thread_list *head, bool force_join);
+
 #endif
